8d941a86980f9b6bfa99d4d257426bd5a0dce2df
Faris Alamin <fma3210@gmail.com>
2020-05-26 13:18:52 +0100

    added controller action for dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
-
+  def dashboard
+    if current_user.manager
+      @sites = Site.where(user: current_user)
+      @projects = Project.where(user: current_user)
+    else
+      @applications = Placement.where(user: current_user)
+      @placements = Placement.where("user_id = ? AND confirmed = ?", current_user.id, true).order(:start_date)
+      @qualifications = UserQualification.where(user: current_user)
+    end
+  end
 end
