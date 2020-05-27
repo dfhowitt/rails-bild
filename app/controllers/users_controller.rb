2535d19fd0ead3f6a4a214f0c634ff40d93ec368
Daniel Howitt <daniel.howittcontact@gmail.com>
2020-05-27 12:58:51 +0100

    added a history partial to dashboard and changed seed file to account for qualifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
       @placements = Placement.joins(:project)
                              .where("placements.user_id = ? AND placements.confirmed = ?", current_user.id, true)
                              .order("projects.start_date")
+      @past_placements = Placement.joins(:project)
+                                  .where("placements.user_id = ? AND projects.end_date < ?", current_user.id, DateTime.now)
+                                  .order("projects.end_date")
       @qualifications = UserQualification.where(user: current_user)
     # end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "destroying everything"
+puts "Destroying everything..."
 
 Project.destroy_all
 Site.destroy_all
 Qualification.destroy_all
 Placement.destroy_all
 User.destroy_all
+Qualification.destroy_all
 
-puts "creating users"
+puts "creating everything :)"
 
 user_names = %w(Faris Chris Gus Dan)
 
@@ -36,7 +37,7 @@ User.create(
   manager: false
   )
 
-site_types = ["Commercial", "Residential", "Industrial"]
+site_types = ['Commercial', 'Residential', 'Industrial']
 locations = ['London', 'Madrid', 'Milan', 'Rome', 'Paris']
 names = ['Vinci', 'Strata', 'Bouygues', 'MITE']
 
@@ -51,19 +52,53 @@ end
 
 sites = Site.all
 
-job_types = ['Labourer', 'Electrictian', 'Asbestos Awareness', 'DBS Labourer', 'Plumber']
+job_types = ['Labourer', 'Skilled Labourer', 'Electrician', 'Asbestos Awareness', 'DBS Labourer']
+qualifications = ['CSCS', 'Blue CSCS', 'ECS', 'Asbestos Awareness', 'DBS']
+
 sites.each do |site|
   5.times do
     res_start_date = Faker::Date.forward(days: rand(1..15))
-    Project.create(
+    index = rand(1..4)
+    project = Project.new(
       start_date: res_start_date,
       end_date: res_start_date + rand(1..10),
       wage: rand(10..20),
       capacity: rand(1..20),
       description: Faker::Cannabis.health_benefit,
       site_id: site.id,
-      job_type: job_types.sample
+      job_type: job_types[index]
       )
+    project.save
+    ProjectQualification.create(
+      project_id: project.id,
+      qualification_id: qualification[index]
+    )
   end
 end
 
+qualifications.each do |qualification|
+  Qualification.create(
+    name: qualification
+    )
+end
+
+User.all.each do |user|
+  UserQualification.create(
+    user_id: user.id,
+    qualification_id: 1
+  )
+end
+
+User.all.each do |user|
+  UserQualification.create(
+    user_id: user.id,
+    qualification_id: qualifications.except('CSCS').sample
+    )
+end
+
+Project.all.each do |project|
+  ProjectQualification.create(
+    project_id: project.id,
+    qualification_id: 1
+    )
+end
