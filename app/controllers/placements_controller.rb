a691c7faf2eab0d18844cf367ab97a659bbda1d2
Faris Alamin <fma3210@gmail.com>
2020-05-27 12:29:30 +0100

    Added manager confirmations for worker placements with basic styling

--- app/controllers/placements_controller.rb
+++ app/controllers/placements_controller.rb
@@ -5,4 +5,10 @@ class PlacementsController < ApplicationController
     @placement.project = Project.find(params[:project_id])
     @placement.save
   end
+
+  def update
+    @placement = Placement.find(params[:id])
+    @placement.confirmed = true
+    @placement.save
+  end
 end
