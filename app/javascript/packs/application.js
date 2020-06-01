957f7a7b25cac141bf26ca27584f542a051247a7
Gus Threlfall <gus.threlfall@hotmail.com>
2020-06-01 18:02:36 +0100

    sweet alert wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,11 +34,11 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initSweetalert } from '../plugins/init_sweetalert';
 
-// initSweetalert('#sweet-alert-apply', {
-//   title: "Nice job!",
-//   text: "You have successfully applied",
-//   icon: "success"
-// });
+initSweetalert('#sweet-alert-apply', {
+  title: "Nice job!",
+  text: "You have successfully applied",
+  icon: "success"
+});
 
 initSweetalert('#sweet-alert-cancel', {
   title: "Are you sure?",
@@ -58,13 +58,24 @@ initSweetalert('#sweet-alert-cancel', {
         }
 }});
 
-// initSweetalert('#sweet-alert-reject', {
-//   title: "Are you sure?",
-//   // text: "Once ,you will not be able to recover!",
-//   icon: "warning",
-//   buttons: true,
-//   dangerMode: true,
-// })
+initSweetalert('#sweet-alert-reject', {
+  title: "Are you sure?",
+  // text: "Once ,you will not be able to recover!",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+
+}, (id) => {
+   console.log(id)
+   return (value) => {
+        console.log(value)
+        if (value) {
+          const link = document.querySelector(`#reject-link-${id}`);
+          link.click();
+          value = false
+        }
+}});
+
 
 
 // .then((willDelete) => {
