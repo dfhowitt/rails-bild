dcc5221491a54c3704c44fd2b042fefd71b76b11
alexander haumer <alexander.haumer@me.com>
2020-06-03 13:13:13 +0100

    sweetalert upgrade to sweetalert2

--- db/schema.rb
+++ db/schema.rb
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_140805) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone_number"
-    t.boolean "manager"
+    t.boolean "manager", default: false
     t.string "first_name"
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
