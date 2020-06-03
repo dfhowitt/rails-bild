a44afc2f08aad50fc48afabc4300e870c8c36858
Faris Alamin <fma3210@gmail.com>
2020-06-03 17:34:54 +0100

    styled index project card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -65,41 +65,7 @@ const sweetAlert = () => {
             value = false
           }
   }});
-
-  // initSweetalert('#sweet-alert-reject', {
-  //   title: "Are you sure?",
-  //   // text: "Once ,you will not be able to recover!",
-  //   icon: "warning",
-  //   buttons: true,
-  //   dangerMode: true,
-
-  // }, (id) => {
-  //    console.log(id)
-  //    return (value) => {
-  //         console.log(value)
-  //         if (value) {
-  //           const link = document.querySelector(`#reject-link-${id}`);
-  //           link.click();
-  //           value = false
-  //         }
-  // }});
-
-
-
-  // .then((willDelete) => {
-  //   if (willDelete) {
-  //     swal("You have succesfully deleted", {
-  //       icon: "success",
-  //     });
-  //   } else {
-  //     swal("Delete cancelled!");
-  //   }
-  // });
-  }
-
-
-
-
+}
 
 import { clickableCheckbox } from '../checkbox';
 
